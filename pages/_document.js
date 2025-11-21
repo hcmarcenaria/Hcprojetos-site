85a37a05c052f27c940d2fb21a28fe5e41bcf5e4
hcmarcenaria <hcmarcenariaemovelaria@hotmail.com>
2025-11-21 20:02:38 -0300

    Atualizar o _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,15 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head>
-        {/* Código do Google AdSense */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8678259064478046"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
-
+      <Head />
       <body>
         <Main />
         <NextScript />
