03e43f59a9ba52501b3f14593106ec8d167c723c
hcmarcenaria <hcmarcenariaemovelaria@hotmail.com>
2025-11-21 20:10:39 -0300

    Atualizar o _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,5 @@
-import '@/styles/globals.css'
-import Script from 'next/script'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      {/* Código do Google AdSense */}
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8672859609478046"
-        crossOrigin="anonymous"
-        strategy="beforeInteractive"
-      />
-
-      {/* Seu site */}
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
